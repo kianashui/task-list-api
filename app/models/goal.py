4cc1c9f93e7740ce888a1b0c230882865e22bc88
Kiana <kianashui@gmail.com>
2022-05-09 08:53:10 -0700

    updated goal and task models to reflect one goal to many task relationship

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,5 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", back_populates="goal")
+
--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,5 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
+    goal = db.relationship("Goal", back_populates="tasks")
